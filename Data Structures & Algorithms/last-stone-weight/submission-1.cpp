502d3a7dfd9a06109b566a0659f92b76880f6f78
Masthanaiah-C <67196171+Masthanaiah-C@users.noreply.github.com>
2026-04-30 16:38:25 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        std::priority_queue<int> pq;
+        for (auto stone: stones) {
+            pq.push(stone);
+        }
+
+        while(pq.size()> 1) {
+            int first = pq.top();
+            pq.pop();
+            int second = pq.top();
+            pq.pop();
+
+            if (first == second) continue;
+            pq.push(abs(first-second));
+
+        }
+        if (pq.size() == 0) return 0;
+        return pq.top();
+    }
+};
